09c258866863c5b7a5f796f37dd9720aee75559b
ChirikQA <chirikartemwrk@gmail.com>
2024-04-15 17:49:48 +0300

    All corrtect tests

--- src/test/java/com/example/chinamotors_project/MainPageTest.java
+++ src/test/java/com/example/chinamotors_project/MainPageTest.java
@@ -1,21 +1,17 @@
 package com.example.chinamotors_project;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
-    MainPage mainPage = new MainPage();
 
     @BeforeAll
     public static void setUpAll() {
