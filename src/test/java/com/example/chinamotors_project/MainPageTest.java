dbabb202a2f49658b62ef999689f506729a64443
ChirikQA <chirikartemwrk@gmail.com>
2024-05-06 14:01:17 +0300

    Add tests - testButton_slider_card_sedan - testButton_slider_card_hetchbek - testButton_slider_card_liftbek - testButton_slider_card_vnedorojnik - testButton_slider_card_mikroavtobus - testButton_slider_card_universal - testButton_slider_card_cupe Add functions - collectUniqueCardUrls - collectCardUrls - isNextPageButtonActive

--- src/test/java/com/example/chinamotors_project/MainPageTest.java
+++ src/test/java/com/example/chinamotors_project/MainPageTest.java
@@ -1,21 +1,23 @@
 package com.example.chinamotors_project;
+
 import com.codeborne.selenide.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainPageTest {
 
@@ -28,8 +30,9 @@ public class MainPageTest {
     }
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         open("https://china-motors.org/");
+        Configuration.timeout = 10000; // 10 секунд
     }
 
     @Test
@@ -128,7 +131,7 @@ public class MainPageTest {
     }
 
     @Test
-    public void click_new_car_name(){
+    public void click_new_car_name() {
         $(By.xpath("/html/body/div[2]/div[1]/div[1]/div/section[1]/div/div[2]/div[1]/a[1]/div/div[2]/div[1]")).click();
         sleep(2000);
         String expectedUrl = "https://china-motors.org/products/paladin-2nk";
@@ -137,7 +140,7 @@ public class MainPageTest {
     }
 
     @Test
-    public void click_newBig_car_img(){
+    public void click_newBig_car_img() {
         $(By.xpath("/html/body/div[2]/div[1]/div[1]/div/section[1]/div/div[2]/div[2]/a[1]/div/div[2]")).click();
         sleep(2000);
         String expectedUrl = "https://china-motors.org/products/monjaro-sds";
@@ -146,7 +149,7 @@ public class MainPageTest {
     }
 
     @Test
-    public void click_newBig_car_name(){
+    public void click_newBig_car_name() {
         $(By.xpath("/html/body/div[2]/div[1]/div[1]/div/section[1]/div/div[2]/div[2]/a[1]/div/div[1]/div[1]")).click();
         sleep(2000);
         String expectedUrl = "https://china-motors.org/products/monjaro-sds";
@@ -155,7 +158,7 @@ public class MainPageTest {
     }
 
     @Test
-    public void click_new_car_img(){
+    public void click_new_car_img() {
         $(By.xpath("/html/body/div[2]/div[1]/div[1]/div/section[1]/div/div[2]/div[1]/a[1]/div/div[1]/img")).click();
         sleep(2000);
         String expectedUrl = "https://china-motors.org/products/paladin-2nk";
@@ -164,7 +167,7 @@ public class MainPageTest {
     }
 
     @Test
-    public void new_carBlock(){
+    public void new_carBlock() {
         $$(".newcars_row .row_block").shouldHave(CollectionCondition.size(10));
 
         for (SelenideElement selenideElement : $$(".newcars_row .row_block")) {
@@ -188,16 +191,263 @@ public class MainPageTest {
         }
     }
 
-    private int getStatusCode(String urlString) {
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            return connection.getResponseCode();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
+    @Test
+    public void testButton_slider_card_sedan() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "sedan?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/sedan");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+    @Test
+    public void testButton_slider_card_hetchbek() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "hetchbek?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/hetchbek");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+    @Test
+    public void testButton_slider_card_liftbek() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "liftbek?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/liftbek");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+    @Test
+    public void testButton_slider_card_vnedorojnik() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "vnedorojnik?page=";
+        boolean allCardsHaveSlider = true;
+
+        // Вывод марки с таймаутом
+        System.out.println("Таймаут Selenide установлен в: " + Configuration.timeout + " миллисекунд");
+
+        open("https://china-motors.org/all/catagory/vnedorojnik");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+    @Test
+    public void testButton_slider_card_mikroavtobus() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "mikroavtobus?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/mikroavtobus");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+    @Test
+    public void testButton_slider_card_universal() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "universal?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/universal");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+    @Test
+    public void testButton_slider_card_cupe() {
+        Set<String> uniqueCardUrls = new HashSet<>();
+        String mark = "cupe?page=";
+        boolean allCardsHaveSlider = true;
+
+        open("https://china-motors.org/all/catagory/cupe");
+
+        collectUniqueCardUrls(uniqueCardUrls, mark);
+
+        for (String cardUrl : uniqueCardUrls) {
+            try {
+                open(cardUrl);
+
+                $(".slider-container").shouldBe(visible);
+
+                if (!($(".swiper-button-prev").exists() && $(".swiper-button-next").exists())) {
+                    allCardsHaveSlider = false;
+                    System.out.println("Карточка товара " + cardUrl + " не содержит кнопок слайдера.");
+                }
+
+                back();
+            } catch (TimeoutException e) {
+                System.out.println("Таймаут произошел при загрузке карточки товара с маркой: " + cardUrl);
+            }
+        }
+
+        assertTrue(allCardsHaveSlider);
+    }
+
+
+    private void collectUniqueCardUrls(Set<String> uniqueCardUrls, String mark) {
+        int currentPage = 1;
+        String currentPageUrl = "https://china-motors.org/all/catagory/" + mark + currentPage;
+
+        open(currentPageUrl);
+        boolean hasPagination = $(".pagination_newnli").exists();
+        int cardsOnCurrentPage;
+        if (hasPagination) {
+            while (true) {
+                cardsOnCurrentPage = collectCardUrls(uniqueCardUrls);
+                System.out.println("Текущая страница: " + currentPage);
+                System.out.println("Количество карточек: " + cardsOnCurrentPage / 4);
+                System.out.println("Общее количество карточек: " + uniqueCardUrls.size());
+
+                if (!isNextPageButtonActive(currentPageUrl)) {
+                    break;
+                }
+
+                currentPage++;
+                currentPageUrl = "https://china-motors.org/all/catagory/" + mark + currentPage;
+                open(currentPageUrl);
+            }
+        } else {
+            cardsOnCurrentPage = collectCardUrls(uniqueCardUrls);
+            System.out.println("Текущая страница: " + currentPage);
+            System.out.println("Количество карточек: " + cardsOnCurrentPage / 4);
+            System.out.println("Общее количество карточек: " + uniqueCardUrls.size());
         }
     }
+
+
+    private int collectCardUrls(Set<String> uniqueCardUrls) {
+        ElementsCollection productCardLinks = $$(".content_info_list_a");
+        for (SelenideElement element : productCardLinks) {
+            uniqueCardUrls.add(element.getAttribute("href"));
+        }
+        return productCardLinks.size();
+    }
+
+
+    private boolean isNextPageButtonActive(String nextPageUrl) {
+        open(nextPageUrl);
+        $(".content_info_list_a").shouldBe(visible);
+        return !$(".pagination_newnli.last-newli[aria-disabled='true']").exists();
+    }
+
 }
